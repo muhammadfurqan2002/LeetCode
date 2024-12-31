2d7842322ada5604c5466f084706bde7cdbf459d
Muhammad Furqan <furqan.b2002@gmail.com>
2025-01-01 02:23:25 +0500

    Find Single Number

---
+++ SingleNumber.java
@@ -0,0 +1,13 @@
+class SingleNumber {
+    public static int singleNumber(int[] nums) {
+        int num=0;
+        for(int i=0;i<nums.length;i++){
+            num^=nums[i];
+        }
+        return num;
+    }
+    public static void main(String[] args) {
+        int nums={4,1,2,1,2};
+        System.out.println(singleNumber(nums));
+    }
+}
