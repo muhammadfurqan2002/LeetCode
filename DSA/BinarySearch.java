58f160fb4ae6e6cfe39b0868c623420acbc99fa9
Muhammad Furqan <furqan.b2002@gmail.com>
2025-06-02 02:40:45 +0500

    Binary Search

---
+++ DSA/BinarySearch.java
@@ -0,0 +1,37 @@
+public class BinarySearch {
+    public static int _search(int[]array,int target){
+        if (array.length==0) {
+            System.out.println("Array is empty");
+            return -1;
+        }
+        if(array[0]==target)return 0;
+        if(array[array.length-1]==target)return array.length-1;
+        if(array[0]>target || array[array.length-1]<target){
+            System.out.println("Element not found");
+            return -1;
+        }
+        int start=0,end=array.length-1;
+        int mid=start+(end-start)/2;
+        while (start<=end) {
+            if(array[mid]==target){
+                return mid;
+            } else if(array[mid]<target){
+                start=mid+1;
+            } else {
+                end=mid-1;
+            }
+        }
+        System.out.println("Element not found");
+        return -1;
+    }
+    public static void main(String[] args) {
+        int [] array = {2, 3, 4, 10, 40};
+        int target = 10;
+        int result = _search(array, target);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
